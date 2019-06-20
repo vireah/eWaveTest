e9634223bfbb47a2187698f6cfb58277f576cd80
vireah <vireah@gmail.com>
2019-06-20 20:01:54 +0300

    feat: add save in localStorage

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,47 @@
 
 const main = document.getElementById("main");
-let items = [];
-items.push({film:"The Godfather", rating:2}, {film:"The dark knight", rating:4}, {film:"12 angry man", rating:1});
-for(let i = 0; i < items.length; i++) {
-    const item = items[i].film;
-    const rating = items[i].rating;
-    
-    //console.log(item,rating);
-    createItem(item,rating);
-    
+
+if(!localStorage.getItem("raiting")){
+    let items = [];
+    items.push({film:"The Godfather", rating:0}, {film:"The dark knight", rating:0}, {film:"12 angry man", rating:0});
+    saveState(items);
+   // buildPage(items);
 }
 
+buildPage();
+
+function saveState(items) {
+    alert(items);
+    var serialObj = JSON.stringify(items);
+    localStorage.setItem("raiting", serialObj);
+    //var returnObj = JSON.parse(localStorage.getItem("raiting"));
+    //items = returnObj;
+    //buildPage(items);
+}
+
+function getState(){
+    var returnObj = JSON.parse(localStorage.getItem("raiting"));
+    return items = returnObj;
+}
+
+function buildPage(items){
+    items = getState();
+    console.log(getState());
+    for(let i = 0; i < items.length; i++) {
+        const item = items[i].film;
+        const rating = items[i].rating;
+        
+        //console.log(item,rating);
+        createItem(item,rating); 
+    }
+}
 function createItem(item,rating) {
-    const searchButton = document.createElement('div');
-    main.appendChild(searchButton);
-    searchButton.innerHTML = `
-    <div id="film-iteam">
-        <div >
-        ${item}
-        </div>
+    const film_box = document.createElement('div');
+    film_box.classList.add('item');  
+    main.appendChild(film_box);
+    film_box.innerHTML = `
+    <div id="film-iteam" class ="film-iteam">
+        <div><p>${item}</p></div>
         
         <div id="star-rating" class="star-rating">
         <div class="star-rating__wrap">
@@ -57,6 +80,9 @@ star.addEventListener('click', function(e){
             if(items[i].film === film){
                 old_rating = items[i].rating;
                 items[i].rating = rating;
+                
+                var serialObj = JSON.stringify(items);
+                localStorage.setItem("raiting", serialObj);
             }
         }
         deleteStar(film, old_rating);
@@ -66,7 +92,7 @@ star.addEventListener('click', function(e){
             star.classList.add('star-rating__input_checked');  
         }
         console.log(items);
-           
+       
     }
     
  //   console.log(e.target);
@@ -75,37 +101,20 @@ star.addEventListener('click', function(e){
 }, true);
 
 function deleteStar (item, old_rating){
-    
     for(let i = 1; i<=old_rating; i++){
         let star = document.getElementById(`${item}-${i}`);
-        console.log(star.classList,"before delete");   
-        //console.log( star.classList);
         star.classList.remove("star-rating__input_checked");
-        console.log(star.classList,"after delete");  
     }
-
-
 }
+function compareRating(film, film) {
+    return film.rating - film.rating;
+  }
 
-console.log(document.getElementById('star-rating-1').className.split(/\s+/));
-console.log(document.getElementById('star-rating-1').className.split(/\s+/));
+const add = document.getElementById("add_button");
 
-const add = document.getElementById("add");
 add.addEventListener('click', function(e){
-    const searchButton = document.createElement('div');
-    main.appendChild(searchButton);
-    searchButton.innerHTML = `<div class="star-rating">
-    <div class="star-rating__wrap">
-    <input class="star-rating__input" id="star-rating-5" type="radio" name="rating" value="5">
-    <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-5" title="5 out of 5 stars"></label>
-    <input class="star-rating__input" id="star-rating-4" type="radio" name="rating" value="4">
-    <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-4" title="4 out of 5 stars"></label>
-    <input class="star-rating__input" id="star-rating-3" type="radio" name="rating" value="3">
-    <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-3" title="3 out of 5 stars"></label>
-    <input class="star-rating__input" id="star-rating-2" type="radio" name="rating" value="2">
-    <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-2" title="2 out of 5 stars"></label>
-    <input class="star-rating__input" id="star-rating-1" type="radio" name="rating" value="1">
-    <label class="star-rating__ico fa fa-star-o fa-lg" for="star-rating-1" title="1 out of 5 stars"></label>
-    </div>`;
-
+    const value = document.getElementById("add_value").value;
+    //const new_item = e.target.value;
+    items.push({film: value, rating:0});
+    createItem(value,0);
 }, true);
